352f42eeae25ff45fffb3980b58a97e2f4ca472d
nonate-tw <nonate@thoughtworks.com>
2018-09-07 00:22:57 -0300

    first commit

---
+++ src/main/java/com/microservices1/Microservices1Application.java
@@ -0,0 +1,12 @@
+package com.microservices1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservices1Application.class, args);
+	}
+}
